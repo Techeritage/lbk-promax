a6b090fbfb60a7c5ce605cd0564f60194bfa1af2
Techeritage <fatomoyeemmanuel1@gmail.com>
2023-12-17 21:45:29 +0100

    Update script.js

--- script.js
+++ script.js
@@ -9,3 +9,10 @@ const image = document.querySelector('.viewimg');
 setTimeout(() => {
   image.classList.add('active')
 }, 1500);
+
+//copyright date function
+const yearDisplay = document.getElementId("year");
+const date = new Date();
+const year = date.getFullYear():
+
+yearDisplay.innerHTML = year;
